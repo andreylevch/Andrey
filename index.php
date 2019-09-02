9f9446d6c6e79ba1cf4a86d85141f5c416870f3c
andreylevch <alexwm@ua.fm>
2019-09-02 16:38:42 +0300

    read

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
     <head>
 	<title>My Site</title>
     </head>
-    <body>
-	<form>
-	<h1>Galerry</h1>
-	<img src="/foto/1.jpg">
-	<img src="/foto/2.jpeg">
-	</form>
+    <body>	
+        <ul>
+            <li>
+                <a href="index.php">News</a>
+            </li>
+            <li>
+                <a href="photo_gallery.php">Gallery</a>
+            </li>
+        </ul>
+        <p><a href="https://google.com" target="_blank">Google</a>studio.2019</p>
     </body>
 </html>
 
